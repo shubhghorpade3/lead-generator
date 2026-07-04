b546b5c4e9e754b95d1c2c53e60478f57b9077f9
shubhghorpade3 <shubhghorpade3@gmail.com>
2026-07-04 14:55:29 -0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Lead Generator API</h1>
+      <p>If you can see this, deployment works 🎉</p>
+      <p>Next step: API endpoint /api/leads</p>
+    </main>
+  );
+}
